7693e4caa67e7394fc501dfafa227e18ef0805d9
Krami97 <tin.kramaric@gmail.com>
2024-10-08 20:02:18 +0200

    pocetak kalkulatora , unesi dva broja

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("unesi prvi cjeli broj: ");
+        System.out.println("unesi drugi cijeli broj: ");
+        }
+    }
