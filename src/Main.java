2e5687e9b41d8f9e3ab68f1c4e11169831ad0b40
Krami97 <tin.kramaric@gmail.com>
2024-10-08 20:06:02 +0200

    dodano zbrajanje

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("unesi prvi cjeli broj: ");
+        int number1 = scanner.nextInt();
         System.out.println("unesi drugi cijeli broj: ");
+        int number2 = scanner.nextInt();
+
+        var suma = number2 + number1;
+        System.out.println("Zbroj=" + suma);
+
         }
     }
