667f1a7a11e46044ddaa1940c14006b238cf6b22
Krami97 <tin.kramaric@gmail.com>
2024-10-09 19:08:02 +0200

    program koji provjerava koliko parnih i neparnih od 1 do 100

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0;i<=100;i++){
-            System.out.println(i);
-        }
 
+        int odd_num = 0;
+        int even_num = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                even_num += 1;
+            } else {
+                odd_num += 1;
+            }
 
         }
+        System.out.println("parnih brojva je : " + even_num);
+        System.out.println("neparnih brojeva je : " + odd_num);
     }
+
+}
